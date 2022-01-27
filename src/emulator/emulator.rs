87a86c2a25d749cd1bcad3f6eaf36e9bd5820640
AdoKa3710 <adicoolwest@gmail.com>
2022-01-28 00:26:01 +0600

    Add flag

--- src/emulator/emulator.rs
+++ src/emulator/emulator.rs
@@ -18,6 +18,7 @@ pub struct Emulator {
     logcat_output: Option<PathBuf>,
     partition_size: Option<u32>,
     cache: Option<PathBuf>,
+    cache_size: Option<u32>,
     no_cache: bool,
     nocache: bool,
     sdcard: Option<PathBuf>,
@@ -46,7 +47,7 @@ pub struct Emulator {
     accel: Option<String>,
     no_accel: bool, // TODO
     ranchu: bool, // TODO
-    engine: Option<PathBuf>, // TODO
+    engine: Option<String>, // TODO
     netspeed: Option<u32>,
     netdelay: Option<String>, // TODO
     netfast: bool, // TODO
@@ -172,8 +173,278 @@ impl Emulator {
         }
     }
 
+    /// List available AVDs
     pub fn list_avds(&mut self, list_avds: bool) -> &mut Self {
         self.list_avds = list_avds;
+        self
+    }
+
+    /// Search for system disk images in <dir>
+    pub fn sysdir(&mut self, sysdir: &Path) -> &mut Self {
+        self.sysdir = Some(sysdir.to_owned());
+        self
+    }
+
+    /// Read initial system image from <file>
+    pub fn system(&mut self, system: &Path) -> &mut Self {
+        self.system = Some(system.to_owned());
+        self
+    }
+
+    /// Read initial vendor image from <file>
+    pub fn vendor(&mut self, vendor: &Path) -> &mut Self {
+        self.vendor = Some(vendor.to_owned());
+        self
+    }
+
+    /// Make system & vendor image writable after `adb remount`
+    pub fn writable_system(&mut self, writable_system: bool) -> &mut Self {
+        self.writable_system = writable_system;
+        self
+    }
+
+    /// Delay adb communication till boot completes
+    pub fn delay_adb(&mut self, delay_adb: bool) -> &mut Self {
+        self.delay_adb = delay_adb;
+        self
+    }
+
+    /// Write user data into <dir>
+    pub fn datadir(&mut self, datadir: &Path) -> &mut Self {
+        self.datadir = Some(datadir.to_owned());
+        self
+    }
+
+    /// Use specific emulated kernel
+    pub fn kernel(&mut self, kernel: &Path) -> &mut Self {
+        self.kernel = Some(kernel.to_owned());
+        self
+    }
+
+    /// Ramdisk image (default <system>/ramdisk.img)
+    pub fn ramdisk(&mut self, ramdisk: &Path) -> &mut Self {
+        self.ramdisk = Some(ramdisk.to_owned());
+        self
+    }
+
+    /// Obsolete, use -system <file> instead
+    pub fn image(&mut self, image: &Path) -> &mut Self {
+        self.image = Some(image.to_owned());
+        self
+    }
+
+    /// Same as `-init-data <file>`
+    pub fn initdata(&mut self, initdata: &Path) -> &mut Self {
+        self.initdata = Some(initdata.to_owned());
+        self
+    }
+
+    /// Data image (default <datadir>/userdata-qemu.img)
+    pub fn data(&mut self, data: &Path) -> &mut Self {
+        self.data = Some(data.to_owned());
+        self
+    }
+
+    /// Read initial encryption key image from <file>
+    pub fn encryption_key(&mut self, encryption_key: &Path) -> &mut Self {
+        self.encryption_key = Some(encryption_key.to_owned());
+        self
+    }
+
+    /// Output file of logcat(default none)
+    pub fn logcat_output(&mut self, logcat_output: &Path) -> &mut Self {
+        self.logcat_output = Some(logcat_output.to_owned());
+        self
+    }
+
+    /// System/data partition size in MBs
+    pub fn partition_size(&mut self, partition_size: u32) -> &mut Self {
+        self.partition_size = Some(partition_size);
+        self
+    }
+
+    /// Cache partition image (default is temporary file)
+    pub fn cache(&mut self, cache: &Path) -> &mut Self {
+        self.cache = Some(cache.to_owned());
+        self
+    }
+
+    /// Cache partition size in MBs
+    pub fn cache_size(&mut self, cache_size: u32) -> &mut Self {
+        self.cache_size = Some(cache_size);
+        self
+    }
+
+    /// Disable the cache partition
+    pub fn no_cache(&mut self, no_cache: bool) -> &mut Self {
+        self.no_cache = no_cache;
+        self
+    }
+
+    /// Same as -no-cache
+    pub fn nocache(&mut self, nocache: bool) -> &mut Self {
+        self.nocache = nocache;
+        self
+    }
+
+    /// SD card image (default <datadir>/sdcard.img)
+    pub fn sdcard(&mut self, sdcard: &Path) -> &mut Self {
+        self.sdcard = Some(sdcard.to_owned());
+        self
+    }
+
+    /// Qeuit emulator after guest boots completely, or after timeout in seconds
+    pub fn quit_after_boot(&mut self, quit_after_boot: u32) -> &mut Self {
+        self.quit_after_boot = Some(quit_after_boot);
+        self
+    }
+
+    /// Use the emulator in the specified dir (relative or absolute path)v
+    pub fn qemu_top_dir(&mut self, qemu_top_dir: &Path) -> &mut Self {
+        self.qemu_top_dir = Some(qemu_top_dir.to_owned());
+        self
+    }
+
+    /// Monitor the adb messages between guest and host, default not
+    pub fn monitor_adb(&mut self, monitor_adb: bool) -> &mut Self {
+        self.monitor_adb = monitor_adb;
+        self
+    }
+
+    /// File that contains all state snapshots (default <datadir>/snapshots.img)
+    pub fn snapstorage(&mut self, snapstorage: &Path) -> &mut Self {
+        self.snapstorage = Some(snapstorage.to_owned());
+        self
+    }
+
+    /// Do not mount a snapshot storage file (this disables all snapshot functionality)
+    pub fn no_snapstorage(&mut self, no_snapstorage: bool) -> &mut Self {
+        self.no_snapstorage = no_snapstorage;
+        self
+    }
+
+    /// Name of snapshot within storage file for auto-start and auto-save (default 'default-boot')
+    pub fn snapshot(&mut self, snapshot: String) -> &mut Self {
+        self.snapshot = Some(snapshot);
+        self
+    }
+
+    /// Perform a full boot and do not auto-save, but qemu vmload and vmsave operate on snapstorage
+    pub fn no_snapshot(&mut self, no_snapshot: bool) -> &mut Self {
+        self.no_snapshot = no_snapshot;
+        self
+    }
+
+    /// Do not auto-save to snapshot on exit: abandon changed state
+    pub fn no_snapshot_save(&mut self, no_snapshot_save: bool) -> &mut Self {
+        self.no_snapshot_save = no_snapshot_save;
+        self
+    }
+
+    /// Do not auto-start from snapshot: perform a full boot
+    pub fn no_snapshot_load(&mut self, no_snapshot_load: bool) -> &mut Self {
+        self.no_snapshot_load = no_snapshot_load;
+        self
+    }
+
+    /// Show a list of available snapshots
+    pub fn snapshot_list(&mut self, snapshot_list: bool) -> &mut Self {
+        self.snapshot_list = snapshot_list;
+        self
+    }
+
+    /// Do not try to correct snapshot time on restore
+    pub fn no_snapshot_update_time(&mut self, no_snapshot_update_time: bool) -> &mut Self {
+        self.no_snapshot_update_time = no_snapshot_update_time;
+        self
+    }
+
+    /// Reset the user data image (copy it from initdata)
+    pub fn wipe_data(&mut self, wipe_data: bool) -> &mut Self {
+        self.wipe_data = wipe_data;
+        self
+    }
+
+    /// Use a specific android virtual device
+    pub fn avd(&mut self, avd: String) -> &mut Self {
+        self.avd = Some(avd);
+        self
+    }
+
+    /// Use a specific target architecture
+    pub fn avd_arch(&mut self, avd_arch: String) -> &mut Self {
+        self.avd_arch = Some(avd_arch);
+        self
+    }
+
+    /// Search skins in <dir> (default <system>/skins)
+    pub fn skindir(&mut self, skindir: &Path) -> &mut Self {
+        self.skindir = Some(skindir.to_owned());
+        self
+    }
+
+    /// Select a given skin
+    pub fn skin(&mut self, skin: String) -> &mut Self {
+        self.skin = Some(skin);
+        self
+    }
+
+    /// Deprecated: create an AVD with no skin instead
+    pub fn no_skin(&mut self, no_skin: bool) -> &mut Self {
+        self.no_skin = no_skin;
+        self
+    }
+
+    /// Same as -no-skin
+    pub fn noskin(&mut self, noskin: bool) -> &mut Self {
+        self.noskin = noskin;
+        self
+    }
+
+    /// Physical RAM size in MBs
+    pub fn memory(&mut self, memory: u32) -> &mut Self {
+        self.memory = Some(memory);
+        self
+    }
+
+    /// Run only the UI feature requested
+    pub fn ui_only(&mut self, ui_only: String) -> &mut Self {
+        self.ui_only = Some(ui_only);
+        self
+    }
+
+    /// Assign an id to this virtual device (separate from the avd name)
+    pub fn id(&mut self, id: String) -> &mut Self {
+        self.id = Some(id);
+        self
+    }
+
+    /// Set number of CPU cores to emulator
+    pub fn cores(&mut self, cores: u32) -> &mut Self {
+        self.cores = Some(cores);
+        self
+    }
+
+    /// Configure emulation acceleration
+    pub fn accel(&mut self, accel: String) -> &mut Self {
+        self.accel = Some(accel);
+        self
+    }
+
+    /// Same as '-accel off'
+    pub fn no_accel(&mut self, no_accel: bool) -> &mut Self {
+        self.no_accel = no_accel;
+        self
+    }
+
+    /// Use new emulator backend instead of the classic one
+    pub fn ranchu(&mut self, ranchu: bool) -> &mut Self {
+        self.ranchu = ranchu;
+        self
+    }
+
+    pub fn engine(&mut self, engine: String) -> &mut Self {
+        self.engine = Some(engine);
         self
     }
 }
