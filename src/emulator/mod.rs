7a75483600d003884b01a824a2a36b78112c723f
AdoKa3710 <adicoolwest@gmail.com>
2022-02-01 18:17:04 +0600

    Finish write emulator wraper

--- src/emulator/mod.rs
+++ src/emulator/mod.rs
@@ -1,3 +1,12 @@
 mod emulator;
 
 pub use emulator::*;
+
+#[derive(Clone, Copy)]
+pub struct Emulator;
+
+impl Emulator {
+    pub fn emulator(self) -> EmulatorTools {
+        EmulatorTools::new()
+    }
+}
