ab3ca1a79f36a9cb7a70142b978c6718ee139126
Адиль Кулмурзин <Adoka3710>
2022-03-09 18:06:46 +0600

    Fix dumpsys

--- src/adb/adb_shell_dumpsys.rs
+++ src/adb/adb_shell_dumpsys.rs
@@ -1,8 +1,25 @@
 use crate::error::*;
 use std::process::Command;
 
+/// ## Dumpsys
+/// 
+/// dumpsys is a tool that runs on Android devices and provides information about
+/// system services. You can call dumpsys from the command line using the Android
+/// Debug Bridge (ADB) to get diagnostic output for all system services running
+/// on a connected device. This output is typically more verbose than you may
+/// want, so use the command line options described below to get output for only
+/// the system services you're interested in. This page also describes how to use
+/// dumpsys to accomplish common tasks, such as inspecting input, RAM, battery,
+/// or network diagnostics.
 #[derive(Clone, Default)]
 pub struct AdbShellDumpsys {
+    t: Option<String>,
+    help: bool,
+    l: bool,
+    skip: Option<Vec<String>>,
+    service: Option<String>,
+    c: bool,
+    h: bool,
     activity: bool,
     iphonesybinfo: bool,
     battery_set_level: Option<String>,
@@ -16,6 +33,57 @@ impl AdbShellDumpsys {
         Self {
             ..Default::default()
         }
+    }
+
+    /// Specifies the timeout period in seconds. When not specified, the default 
+    /// value is 10 seconds.
+    pub fn t(&mut self, t: String) -> &mut Self {
+        self.t = Some(t);
+        self
+    }
+
+    /// Prints out help text for the dumpsys tool.
+    pub fn help(&mut self, help: bool) -> &mut Self {
+        self.help = help;
+        self
+    }
+
+    /// Outputs a complete list of system services that you can use with dumpsys.
+    pub fn l(&mut self, l: bool) -> &mut Self {
+        self.l = l;
+        self
+    }
+
+    /// Specifies the services that you do not want to include in the output.
+    pub fn skip(&mut self, skip: Vec<String>) -> &mut Self {
+        self.skip = Some(skip);
+        self
+    }
+
+    /// Specifies the service that you want to output. Some services may allow you
+    /// to pass optional arguments. You can learn about these optional arguments
+    /// by passing the -h option with the service, as shown below:
+    ///
+    /// ```xml
+    /// adb shell dumpsys procstats -h
+    /// ```
+    pub fn service(&mut self, service: String) -> &mut Self {
+        self.service = Some(service);
+        self
+    }
+
+    /// When specifying certain services, append this option to output data in a
+    /// machine-friendly format.
+    pub fn c(&mut self, c: bool) -> &mut Self {
+        self.c = c;
+        self
+    }
+
+    /// For certain services, append this option to see help text and additional
+    /// options for that service.
+    pub fn h(&mut self, h: bool) -> &mut Self {
+        self.h = h;
+        self
     }
 
     pub fn activity(&mut self, activity: bool) -> &mut Self {
@@ -53,6 +121,33 @@ impl AdbShellDumpsys {
         let mut dumpsys = Command::new("adb");
         dumpsys.arg("shell");
         dumpsys.arg("dumpsys");
+        if let Some(t) = &self.t {
+            dumpsys.arg("-t").arg(t);
+        }
+        if self.help {
+            dumpsys.arg("--help");
+        }
+        if self.l {
+            dumpsys.arg("-l");
+        }
+        if let Some(skip) = &self.skip {
+            dumpsys.arg("--skip").arg(
+                skip
+                    .iter()
+                    .map(|v| v.to_string())
+                    .collect::<Vec<String>>()
+                    .join(","),
+            );
+        }
+        if let Some(service) = &self.service {
+            dumpsys.arg(service);
+        }
+        if self.c {
+            dumpsys.arg("-c");
+        }
+        if self.h {
+            dumpsys.arg("-h");
+        }
         if self.activity {
             dumpsys.arg("activity");
         }
