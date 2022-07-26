5256457de1a917b1fd73b3b2ab9a2ad1a8636737
unknown <den_9912@mail.ru>
2022-07-26 16:14:38 +0500

    Change android sdk access

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-use error::AndroidError;
 use std::path::PathBuf;
 
 /// On Windows adds `.exe` to given string.
@@ -31,7 +30,33 @@ pub fn sdk_path_from_env() -> crate::error::Result<PathBuf> {
             .ok()
             .or_else(|| std::env::var("ANDROID_SDK_PATH").ok())
             .or_else(|| std::env::var("ANDROID_HOME").ok());
-        std::path::PathBuf::from(sdk_path.ok_or(AndroidError::AndroidSdkNotFound)?)
+        std::path::PathBuf::from(
+            sdk_path.unwrap_or(sdk_install_path()?.to_str().unwrap().to_string()),
+        )
     };
+    Ok(sdk_path)
+}
+
+/// Default installation path
+pub fn sdk_install_path() -> crate::error::Result<PathBuf> {
+    let home_dir_path = dirs::home_dir().unwrap();
+    #[cfg(target_os = "windows")]
+    let path = std::path::Path::new("Local").join("Android").join("Sdk");
+    #[cfg(target_os = "macos")]
+    let path = std::path::Path::new("Library").join("Android").join("sdk");
+    #[cfg(target_os = "linux")]
+    let path = std::path::Path::new("Android").join("sdk");
+
+    #[cfg(target_os = "windows")]
+    let app_data = std::path::Path::new("AppData");
+    #[cfg(target_os = "windows")]
+    let sdk_path = home_dir_path.join(app_data).join(path);
+
+    #[cfg(not(target_os = "windows"))]
+    let sdk_path = home_dir_path.join(path);
+
+    if !sdk_path.exists() {
+        std::fs::create_dir_all(&sdk_path)?;
+    }
     Ok(sdk_path)
 }
